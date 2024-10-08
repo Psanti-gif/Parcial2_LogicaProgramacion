7f14e206f15bead5ce86229cbd600fb1110f85e3
Psanti-gif <psantixt12@gmail.com>
2024-10-07 21:17:01 -0500

    Cambios #1

---
+++ Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool continuar = true;
+        int estudiantesGanaron = 0;
+        int estudiantesPerdieron = 0;
+
+        do
+        {
+            Console.WriteLine("Ingrese el nombre del estudiante:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la nota 1:");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota 2:");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota 3:");
+            double nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota 4:");
+            double nota4 = double.Parse(Console.ReadLine());
+
+            double promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (promedio >= 4.5)
+            {
+                Console.WriteLine($"El estudiante {nombre} esta en la categoria Excelente");
+            }
+
+
+        } while (continuar);
+    }
+}
