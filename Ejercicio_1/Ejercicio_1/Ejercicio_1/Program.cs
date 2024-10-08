8ba82863f52838859cbe1cbfb91e1f1b6cb8281b
Psanti-gif <psantixt12@gmail.com>
2024-10-07 20:40:33 -0500

    Cambios 2

--- Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
@@ -4,7 +4,10 @@
     {
         int opcion;
         bool continuar = true;
-        Random num = new Random();
+        int MaxNum = 0;
+        Random random = new Random();
+        int num = random.Next(0, MaxNum + 1);
+        
 
         do
         {
@@ -13,6 +16,7 @@
             Console.WriteLine("Presiona 1, Para 2 Jugadores ");
             Console.WriteLine("Presiona 2, Para 3 Jugadores ");
             Console.WriteLine("Presiona 3, Para 4 Jugadores ");
+            Console.WriteLine("Presiona 0, Para salir del juego ");
             Console.WriteLine("-------------------------------------------------------");
             opcion = int.Parse(Console.ReadLine());
 
@@ -20,12 +24,33 @@
             {
                 case 0:
                     continuar = false;
+                    Console.WriteLine("Saliendo del juego...");
                     break;
 
                 case 1:
-                    Console.WriteLine("Has seleccionado la opción para 2 jugadores: ");
+                    MaxNum = 50;
+                    num = random.Next(0, MaxNum + 1);
+                    Console.WriteLine("Has seleccionado la opcion de 2 Jugadores:");
+                    Console.WriteLine("A continuación se ha generado un número entre 0 y 50: ");
+                    break;
 
+                case 2:
+                    MaxNum = 100;
+                    num = random.Next(0, MaxNum + 1);
+                    Console.WriteLine("Has seleccionado la opcion de 2 Jugadores:");
+                    Console.WriteLine("Se ha generado un número entre 0 y 50: ");
+                    break;
 
+                case 3:
+                    MaxNum = 200;
+                    num = random.Next(0, MaxNum + 1);
+                    Console.WriteLine("Has seleccionado la opcion de 2 Jugadores:");
+                    Console.WriteLine("Se ha generado un número entre 0 y 50: ");
+                    break;
+
+                default:
+                    continuar = false;
+                    Console.WriteLine("Opción no válida. Saliendo del juego...");
                     break;
 
             }
