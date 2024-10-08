c1b1295df7379a68acf8e8ddca095daee27d1f0b
Psanti-gif <psantixt12@gmail.com>
2024-10-07 21:34:50 -0500

    Ejercicio Finalizado

--- Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
+++ Ejercicio_2/Ejercicio_2/Ejercicio_2/Program.cs
@@ -8,39 +8,82 @@
 
         do
         {
+            Console.WriteLine("");
             Console.WriteLine("Ingrese el nombre del estudiante:");
             string nombre = Console.ReadLine();
+            Console.WriteLine("");
 
+
+            Console.WriteLine("");
             Console.WriteLine("Ingrese la nota 1:");
             double nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
+            Console.WriteLine("");
             Console.WriteLine("Ingrese la nota 2:");
             double nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
+            Console.WriteLine("");
             Console.WriteLine("Ingrese la nota 3:");
             double nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
+            Console.WriteLine("");
             Console.WriteLine("Ingrese la nota 4:");
             double nota4 = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
 
             double promedio = (nota1 + nota2 + nota3 + nota4) / 4;
 
             if (promedio >= 4.5)
             {
-                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nExcelente");
+                Console.WriteLine("");
+                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nEXCELENTE");
+                Console.WriteLine("");
+
             }
             else if (promedio >= 4.0 && promedio <= 4.4)
             {
-                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nSobresaliente");
+                Console.WriteLine("");
+                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nSOBRESALIENTE");
+                Console.WriteLine("");
+
             }
             else if(promedio >= 3.5 && promedio <= 3.9)
             {
-                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nBueno");
+                Console.WriteLine("");
+                Console.WriteLine($"El estudiante {nombre} esta en la categoria: \nBUENO");
+                Console.WriteLine("");
 
             }
             else
             {
-                Console.WriteLine($"El estudiante {nombre} esta en la categoria de: \nInsuficiente");
+                Console.WriteLine("");
+                Console.WriteLine($"El estudiante {nombre} esta en la categoria de: \nINSUFICIENTE");
+                Console.WriteLine("");
+
+            }
+
+            if (promedio >= 3.0)
+            {
+                estudiantesGanaron++;
+            }
+            else
+            {
+                estudiantesPerdieron++;
+            }
+           
+            Console.WriteLine("");
+            Console.WriteLine("¿Desea ingresar otro estudiante? (SI/NO)");
+            string respuesta = Console.ReadLine().ToUpper();
+
+            if (respuesta != "SI")
+            {
+                continuar = false;
+                Console.WriteLine($"\nEstudiantes que ganaron:{estudiantesGanaron}");
+                Console.WriteLine($"\nEstudiantes que perdieron:{estudiantesPerdieron}");
+                Console.WriteLine("");
             }
 
         } while (continuar);
